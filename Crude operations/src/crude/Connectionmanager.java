acdcbe7c4c0287f39b0fab93ea434c428e88aa8f
USER <USER@FACE-85>
2019-12-10 11:55:27 +0530

    project completed

---
+++ Crude operations/src/crude/Connectionmanager.java
@@ -0,0 +1,19 @@
+package crude;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connectionmanager {
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = null;
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "");
+		if (con != null)// connection checking
+		{
+			return con;
+		} else {
+			return null;
+		}
+	}
+}
